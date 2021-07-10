359f22325767fc920bfc75d033f686ab0fbaf513
chentoz <492309597@qq.com>
2021-07-10 11:19:09 +0800

    webpack dev middleware, dev server

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,8 @@ module.exports = {
         //filename: 'bundle.js',
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        clean: true
+        clean: true,
+        publicPath: '/',
     },
     module: {
         rules: [
